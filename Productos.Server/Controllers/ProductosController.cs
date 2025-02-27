563d6c9b58443df2b079d9accf09ec715ce9cf8a
ivancho860916 <ivan860916@gmail.com>
2025-02-27 15:48:24 -0500

    primer commit

---
+++ Productos.Server/Controllers/ProductosController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Productos.Server.Models;
+
+namespace Productos.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductosController : ControllerBase
+    {
+        private readonly ProductosContext context;
+        private ProductosContext _context;
+
+        public ProductosController(ProductosContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        [Route("crear")]
+        public async Task<IActionResult> CrearProducto(Producto producto)
+        {
+            await _context.Productos.AddAsync(producto);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("lista")]
+
+        public async Task<ActionResult<IEnumerable<Producto>>> ListaProductos()
+        {
+            var productos = await _context.Productos.ToListAsync();
+
+            return Ok(productos);
+        }
+
+        [HttpGet]
+        [Route("ver")]
+        public async Task<ActionResult> Verproducto(int id)
+        {
+            Producto producto = await _context.Productos.FindAsync(id);
+
+            if (producto == null)
+            {
+                return NotFound();
+            }
+            return Ok(producto);
+        }
+        [HttpPut]
+        [Route("editar")]
+
+        public async Task<ActionResult> ActualizarProducto(int id, Producto producto)
+        {
+            var productoExistente = await _context.Productos.FindAsync(id);
+
+            productoExistente!.Nombre = producto.Nombre;
+            productoExistente.Descripcion = producto.Descripcion;
+            productoExistente.Precio = producto.Precio;
+
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("eliminar")]
+
+        public async Task<ActionResult> EliminarProducto(int id)
+        {
+            var productoBorrado = await _context.Productos.FindAsync(id);
+
+            _context.Productos.Remove(productoBorrado!);
+
+            await _context.SaveChangesAsync();
+
+            return Ok();
+
+
+        }
+    }
+
+}
